f6088520dca2b16d9ea724900ddae6c3af94e76d
qpeterp <lsepeter0827@naver.com>
2024-07-23 10:10:49 +0900

    Feat :: Save androidId

--- app/src/main/java/com/qpeterp/wising/MainActivity.kt
+++ app/src/main/java/com/qpeterp/wising/MainActivity.kt
@@ -1,17 +1,43 @@
 package com.qpeterp.wising
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.provider.Settings
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.qpeterp.wising.common.Constant
 import com.qpeterp.wising.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    private var androidId: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        getDeviceIdentifier()
 
         setContentView(binding.root)
+    }
 
+    @SuppressLint("HardwareIds")
+    private fun getDeviceIdentifier() {
+        androidId = Settings.Secure.getString(contentResolver, Settings.Secure.ANDROID_ID)
+        handleStart()
+    }
+
+    private fun handleStart() {
+        if (androidId == null) {
+            getDeviceIdentifier()
+            return
+        }
+        Log.d(Constant.TAG, "MainActivity handleStart androidId: $androidId")
+        val sharedPreferences = getSharedPreferences("user_prefs", MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+        editor.putString("androidId", androidId)
+        editor.apply() // 데이터를 비동기적으로 저장
+
+        // 저장된 pref 값 호출 코드
+//        val sharedPreferences = requireActivity().getSharedPreferences("user_prefs", MODE_PRIVATE)
+//        Log.d(Constant.TAG, "PermissionCheckFragment androidId : ${sharedPreferences.getString("androidId", "")}")
     }
 }
--- app/src/main/java/com/qpeterp/wising/ui/StartSplashFragment.kt
+++ app/src/main/java/com/qpeterp/wising/ui/StartSplashFragment.kt
@@ -1,7 +1,6 @@
 package com.qpeterp.wising.ui
 
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -27,9 +26,7 @@ class StartSplashFragment : Fragment() {
         lifecycleScope.launch {
             delay(1500)
 
-            Log.d("StartSplashFragment", "delay 1000 play")
-
-            findNavController().navigate(R.id.action_startSplashFragment_to_startTermFragment)
+            findNavController().navigate(R.id.action_startSplashFragment_to_mainFragment)
         }
 
 
