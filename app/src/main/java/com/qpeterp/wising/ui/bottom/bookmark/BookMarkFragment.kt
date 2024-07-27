9a439ff37c13015944c615edd86ffa3cfe4e786a
qpeterp <lsepeter0827@naver.com>
2024-07-27 17:53:50 +0900

    Feat :: BookMark dialog share

--- app/src/main/java/com/qpeterp/wising/ui/bottom/bookmark/BookMarkFragment.kt
+++ app/src/main/java/com/qpeterp/wising/ui/bottom/bookmark/BookMarkFragment.kt
@@ -2,12 +2,14 @@ package com.qpeterp.wising.ui.bottom.bookmark
 
 import android.app.Dialog
 import android.content.Context.MODE_PRIVATE
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -98,6 +100,26 @@ class BookMarkFragment : Fragment() {
         val authorDetail = dialog.findViewById<TextView>(R.id.authorDetail)
         authorDetail.text = name
 
+        dialog.findViewById<ImageView>(R.id.share).setOnClickListener {
+            share(name, quote)
+        }
+
         dialog.show()
+    }
+
+    private fun share(author: String, content: String) {
+        Log.d(Constant.TAG, "BookMarkDialogFragment share is run")
+        // 공유할 텍스트 생성
+        val shareText = "$content - $author"
+        Log.d(Constant.TAG, "BookMarkDialogFragment shareText is $shareText")
+
+        // 공유 인텐트 생성
+        val shareIntent = Intent(Intent.ACTION_SEND).apply {
+            type = "text/plain"
+            putExtra(Intent.EXTRA_TEXT, shareText)
+        }
+        // 인텐트 선택기를 통해 공유하기
+        val chooser = Intent.createChooser(shareIntent, "공유하기")
+        requireContext().startActivity(chooser)
     }
 }
