0c7ad3ac15580d7b98c42a0902f6a246d308227c
qpeterp <lsepeter0827@naver.com>
2024-09-29 20:12:19 +0900

    Fix :: GetToday quote is empty

--- app/src/main/java/com/qpeterp/wising/ui/main/home/HomeViewModel.kt
+++ app/src/main/java/com/qpeterp/wising/ui/main/home/HomeViewModel.kt
@@ -1,9 +1,11 @@
 package com.qpeterp.wising.ui.main.home
 
 import android.content.SharedPreferences
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.qpeterp.wising.common.Constant
 import com.qpeterp.wising.ui.main.qoutes.BookmarkManager
 
 class HomeViewModel(
@@ -11,7 +13,7 @@ class HomeViewModel(
 ): ViewModel() {
     private var bookmarkManager: BookmarkManager =
         BookmarkManager(sharedPreferences.getString("androidId", "").toString())
-    private var _todayQuote = MutableLiveData<List<String>>()
+    private var _todayQuote = MutableLiveData<List<String>>(listOf("명언 불러오는 중...", ""))
     private var _todayQuoteBookMarkState = MutableLiveData(false)
 
     val todayQuote: LiveData<List<String>>
@@ -30,6 +32,16 @@ class HomeViewModel(
             val editor = sharedPreferences.edit()
             editor.putString("quoteId", bookMarkData.id)
             editor.apply()
+        }
+    }
+
+    fun setTodayQuote(quoteId: String) {
+        bookmarkManager.getQuote(
+            quoteId
+        ) { result ->
+            Log.d(Constant.TAG, "HomeViewModel setTodayQuote(quoteId) result : $result")
+            _todayQuote.value = listOf(result.quote, result.name)
+            Log.d(Constant.TAG, "HomeViewModel setTodayQuote(quoteId) todayQuote : ${todayQuote.value}")
         }
     }
 
